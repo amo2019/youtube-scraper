6304ea3816a4b22f9d017bdec7b692a0f63fdcd2
amo2019 <aminvpython@gmail.com>
2021-04-06 20:10:00 +0200

    edit Header.jsx & add Header.css preparing to replace tailwindcss with pure css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
   return (
-    <header className="mx-auto p-8 bg-orange-700">
-      <div className="menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-2 px-4">
+    <header className="header">
+      <div className="">
         <Link to={{ pathname: "/" }}>
-          <h1 className="text-white text-4xl" label="Back to Home Page">
-            YouTube Scraper (Top 10)
+          <h1 className="" label="Back to Home Page">
+            YouTube Scraper (Top10)
           </h1>
         </Link>
       </div>
