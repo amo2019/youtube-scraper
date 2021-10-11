8677ebd1f9dd7f7779fed66efd4cb5692d372f4b
amo2019 <aminvpython@gmail.com>
2021-10-11 13:43:31 +0200

    refacturing

--- src/components/metrics/Tags.js
+++ src/components/metrics/Tags.js
@@ -3,6 +3,7 @@ import React from "react";
 function Tags(props) {
   if (String(props.tags).trim() === "undefined")
     return <li> no tags exist -</li>;
+
   var tags = props.tags.map(function (tag, i) {
     return <li style={{marginLeft: "0.4rem", paddingLeft: "0.3rem"}} key={tag}> {tag}</li>;
   });
