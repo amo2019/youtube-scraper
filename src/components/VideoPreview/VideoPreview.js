88a17e1436462952c66853d36c0c92f47be16a0d
amo2019 <aminvpython@gmail.com>
2021-10-11 13:33:20 +0200

    add useEffect to prefetch data

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "./VideoPreview.css";
 import { VideoIdContext } from "../../contexts/VideoIdContext";
 import { Link } from "react-router-dom";
@@ -8,13 +8,25 @@ import { youtubeIdUpdate } from "../redux/actions/YoutubeIdActions";
 
 
 function VideoPreview(props) {
-  const [videoId, setVideoId] = useState({
-    idChange: "",
-  });
-  const dispatch = useDispatch();
-  const videoPlayer = useSelector(state => state.layout.videoPlayer);
- 
-  const { setVideoIdChange } = useContext(VideoIdContext);
+
+    const videoPlayer = useSelector(state => state.layout.videoPlayer);
+    const { setVideoIdChange } = useContext(VideoIdContext);
+    const [videoId, setVideoId] = useState({
+      idChange: "",
+    });
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+            try{
+              setVideoId(props.videoId);
+              setVideoIdChange(props.videoId);
+              dispatch(youtubeIdUpdate({ youtubeId: props.videoId }));
+            }
+            catch{
+              console.log("No data..")
+            }
+    }, [props.videoId, setVideoIdChange, dispatch]);
+  
   const handleClick = () => {
     setVideoId(props.videoId);
     setVideoIdChange(props.videoId);
@@ -33,7 +45,7 @@ function VideoPreview(props) {
      setTimeout(()=>{console.log("videoPlayer:", videoPlayer.visible)}, 1000)
     
   }
-  return (
+  return ( 
     
       <div>
         <div className="flex-container">
@@ -43,16 +55,18 @@ function VideoPreview(props) {
             }
             />
           </div>
+          <div onClick={() => handleClick()}>
           <Link to={{ pathname: "/result", videoId: videoId.idChange }}>
           <div>
-            <div>{video.snippet.title}</div>
+            <div >{video.snippet.title}</div>
 
             <p>{video.snippet.channelTitle}</p>
             <div>{description}</div>
            
-            <button className="flex-button"  onClick={() => handleClick(this)}>CLICK FOR DETAILS</button>
+            <button className="flex-button">CLICK FOR DETAILS</button>
           </div>
           </Link>
+          </div>
         </div>
         
       </div>
